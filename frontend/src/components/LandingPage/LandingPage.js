f325cdfde15bf52ece6d6300850ab5accec126aa
LPC <laurencecook80@hotmail.co.uk>
2024-11-22 16:09:49 +0000

    added loader and explanation of Render delay

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -7,6 +7,7 @@ export default function LandingPage () {
             <h1>Welcome!</h1>
             <h3>Thanks for visiting my project.</h3>
             <p>This is a simply-styled, fully-functional e-commerce site built with PostgreSQL, Express, React and Node.</p>
+            <p>To allow the free Render database to spin up please go to the Products page first, then feel free to explore the site.</p>
             <p>You can create an account, or sign in with Google or Facebook, then add products to your basket before checking out. Your order history can then be viewed on your profile page. Test card details are provided for the checkout process.</p>
             <p>Please take a look around the site and feel free to get in touch at <a href="mailto:laurencecook80@hotmail.co.uk">laurencecook80@hotmail.co.uk</a> or reach out on <a href="https://www.linkedin.com/in/laurence-cook-626bb725b">LinkedIn</a>.</p>
         </section>
--- frontend/src/components/Products/Products.js
+++ frontend/src/components/Products/Products.js
@@ -4,12 +4,15 @@ import styles from "./Products.module.css";
 import { Link } from "react-router-dom";
 import { getProducts } from "../../api/api";
 import { productImages } from "../../resources/images/products/images";
+import Loader from "../../resources/styles/Loader";
 
 export default function Products () {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
+            // await new Promise(resolve => setTimeout(resolve, 5000)); // Delay for 5 seconds to test loader
+            // console.log('delayed');
             const productsData = await getProducts();
             setProducts(productsData);
         };
@@ -17,6 +20,8 @@ export default function Products () {
     }, []);
 
     return (
+        <>
+        {products.length > 0 ? 
         <section className={styles.container}>
             {products.map((product, index) => (
                 <Link className={styles.product} to={ROUTES.product(product.id)} key={index}>
@@ -31,7 +36,14 @@ export default function Products () {
             Captain America photo by <a href="https://unsplash.com/@niftyartofficial1_?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">N I F T Y A R T ✍🏻</a> on <a href="https://unsplash.com/photos/captain-america-shield-9HnSQn4TVEg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
             Thor photo by <a href="https://unsplash.com/@wacalke?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mateusz Wacławek</a> on <a href="https://unsplash.com/photos/Tu5cVkWhZXQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
             LARPing photo by <a href="https://unsplash.com/@joetara?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Joseph Ogbonnaya</a> on <a href="https://unsplash.com/photos/a-woman-holding-a-bow-and-arrow-in-a-park-Y0O__TFDOCw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
-            Hulk photo by <a href="https://unsplash.com/@gabrielrana?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Gabriel Tovar</a> on <a href="https://unsplash.com/photos/green-and-black-action-figure-oTKanDGugaA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
+            Hulk photo by <a href="https://unsplash.com/@gabrielrana?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Gabriel Tovar</a> on <a href="https://unsplash.com/photos/green-and-black-action-figure-oTKanDGugaA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+            Loading icon from Uiverse.io by Nawsome</p>
         </section>
+        :
+        <section className={styles.container}>
+            <h2>The free database feature on Render, where this site is hosted, takes up to a minute to spin up. Please be patient whilst it starts, and enjoy the project once it's done!</h2>
+            <Loader />
+        </section>}
+        </>
     )
 };
